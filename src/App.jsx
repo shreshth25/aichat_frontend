b32d1afa7c19ac9a7e0e1a8d33ca42cf58b90249
Shreshth Bansal <shreshthbansal2505@gmail.com>
2024-02-06 11:30:03 +0530

    Code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Layout from "./pages/layout"
+
 import AIChat from "./pages/AIChat"
+import Layout from "./pages/Layout"
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Layout/>}>
+      <Route path="/" element={<Layout />}>
         <Route path="" element={<AIChat/>}></Route>
       </Route>
       </Routes>
